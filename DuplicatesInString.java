52f12fdfe8ec84d5099024075a8cc6dc0d9d1009
Koteswararao8534 <koteswararao8534@gmail.com>
2025-12-30 17:52:47 +0530

    Added Duplicates In String

---
+++ DuplicatesInString.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DuplicatesInString{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String str=sc.next();
+        char arr[]=str.toCharArray();
+     
+
+        for(int i=0;i<arr.length;i++){
+            boolean flag=false;
+            for(int j=0;j<arr.length;j++){
+                if(i!=j && arr[i]==arr[j]){
+                    flag=true;
+                    break;
+                }
+            }
+
+            if(flag)
+                System.out.println(arr[i]);
+        }
+
+    }
+}
