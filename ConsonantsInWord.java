fdb806030b603a6ce5185230b8b894c252664685
Koteswararao8534 <koteswararao8534@gmail.com>
2025-12-31 12:42:15 +0530

    Added ConsonantsInWord

---
+++ ConsonantsInWord.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class ConsonantsInWord{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a Word: ");
+        String str=sc.next();
+
+        char ch[]=str.toLowerCase().toCharArray();
+
+        int count=0;
+
+        for (int i = 0; i < ch.length; i++) {
+            if(!(ch[i]=='a'||ch[i]=='e'||ch[i]=='i'||ch[i]=='o'||ch[i]=='u')){
+                count++;
+            }
+        }
+
+        System.out.println(str+" contains "+count+" consonants");
+    }
+}
