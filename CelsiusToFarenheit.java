f7ec1df41b14b4cb2bf9a7d1f9bab8f3f182081c
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-01-02 19:00:01 +0530

    Fix typo in CelsiusToFahrenheit prompt

--- CelsiusToFarenheit.java
+++ CelsiusToFarenheit.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CelsiusToFarenheit{
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter celcius values: ")
+            System.out.print("Enter celcius values: ");
             double celsius=sc.nextDouble();
             double farenheit=(celsius*9/5)+32;
             System.out.println(celsius+" is equal to "+farenheit);
