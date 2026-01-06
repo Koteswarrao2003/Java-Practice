2e561bbb3ea4a05c1d92ed5c3c17a9c053f099ae
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-01-06 22:52:11 +0530

    Refactor NthSmallestElement for dynamic input and sorting
    Refactored NthSmallestElement to take user input for array size and elements. Improved sorting logic and input validation for smallest element selection.

--- NthSmallestElement.java
+++ NthSmallestElement.java
@@ -1,30 +1,36 @@
 import java.util.*;
 
-public class NthSmallestElement{
+public class NthSmallestElement {
     public static void main(String[] args) {
-        int arr[]={5,6,2,7,1,4,7,96,3,5,82,4};
-
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j]){
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter size of the array: ");
+            int n = sc.nextInt();
+            
+            int[] arr = new int[n];
+            
+            System.out.println("Enter array elements:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[i] > arr[j]) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
                 }
             }
+            
+            System.out.print("Which smallest element you want: ");
+            int small = sc.nextInt();
+            
+            if (small <= 0 || small > arr.length) {
+                System.out.println("Choose value between 1 and " + arr.length);
+            } else {
+                System.out.println(small + " smallest element is: " + arr[small - 1]);
+            }
         }
-
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Which smallest Element You want: ");
-        int small=sc.nextInt();
-
-
-        if(small<=0 || small>arr.length){
-            System.out.println("Choose smallest element in the range of 1 to "+arr.length+" only");
-        }else{
-            System.out.print(small+" smallest element is: ");
-            System.out.println(arr[small-1]);
-        }
-
     }
 }
