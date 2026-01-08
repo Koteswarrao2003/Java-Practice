d08beda2f60f6ccde5679c97c4cd6518ab1e3fa4
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-01-08 20:07:17 +0530

    Update ReversePyramid.java

--- ReversePyramid.java
+++ ReversePyramid.java
@@ -9,7 +9,7 @@ public class ReversePyramid{
             for(int k=1;k<=(2*i-1);k++){
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println()
         }
     }
 }
