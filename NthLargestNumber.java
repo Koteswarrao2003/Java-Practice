384f64753d8361d9e54ddf2515729ed248ce3d23
Koteswararao8534 <koteswararao8534@gmail.com>
2026-01-09 23:06:57 +0530

    Updated nth largest and smallest programs

--- NthLargestNumber.java
+++ NthLargestNumber.java
@@ -1,30 +1,36 @@
 import java.util.*;
 
-public class NthLargestNumber{
+public class NthLargestNumber {
     public static void main(String[] args) {
-        int arr[]={5,6,2,7,1,4,7,96,3,5,82,4};
-
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]>arr[j]){
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter size of the array: ");
+            int n = sc.nextInt();
+            
+            int[] arr = new int[n];
+            
+            System.out.println("Enter array elements:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    if (arr[i] > arr[j]) {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
                 }
             }
+            
+            System.out.print("Which largest element you want: ");
+            int large = sc.nextInt();
+            
+            if (large > arr.length || large <= 0) {
+                System.out.println("Choose value between 1 and " + arr.length);
+            } else {
+                System.out.println(large + " largest element is: " + arr[arr.length - large]);
+            }
         }
-
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Which largest Element You want: ");
-        int large=sc.nextInt();
-
-
-        if(large>arr.length){
-            System.out.println("Choose large element in the range of 1 to "+arr.length+" only");
-        }else{
-            System.out.print(large+" largest element is: ");
-            System.out.println(arr[arr.length-large]);
-        }
-
     }
 }
