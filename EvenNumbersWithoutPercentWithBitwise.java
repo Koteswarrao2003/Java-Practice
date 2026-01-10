4f52d0a77f8e543dac4007d92c2ea4505923fd8c
Koteswararao8534 <koteswararao8534@gmail.com>
2026-01-10 13:00:37 +0530

    Added EvenNumbersWithoutPercentWithBitwise

--- EvenNumbersWithoutPercentWithBitwise.java
+++ EvenNumbersWithoutPercentWithBitwise.java
@@ -1,13 +1,14 @@
-
 import java.util.Scanner;
 
 public class EvenNumbersWithoutPercentWithBitwise {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-        System.out.print("Even numbers up to " + num + " are: ");
-        for (int i = 1; i <= num; i++) {
+        System.out.print("Enter a starting range: ");
+        int start = sc.nextInt();
+        System.out.print("Enter an ending range: ");
+        int end = sc.nextInt();
+        System.out.print("Even numbers from " + start + " to "+end+" are: ");
+        for (int i = start; i <= end; i++) {
             if ((i & 1) == 0) {
                 System.out.print(i + " ");
             }
