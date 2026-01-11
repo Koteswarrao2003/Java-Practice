7f96087eec4506e712818fec4aa2d98be5b03327
Koteswararao8534 <koteswararao8534@gmail.com>
2026-01-11 18:19:20 +0530

    Added RemoveSpacesFromString

--- RemoveSpacesFromString.java
+++ RemoveSpacesFromString.java
@@ -1,16 +1,22 @@
+
+import java.util.Scanner;
+
 public class RemoveSpacesFromString {
     public static void main(String[] args) {
-        String str = "This is a sample string with spaces.";
-        String result="";
-
-        for (int i = 0; i < str.length(); i++) {
-            char currentChar = str.charAt(i);
-            if (currentChar != ' ') {
-                result += currentChar;
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter something: ");
+            String str = sc.nextLine();
+            String result="";
+            
+            for (int i = 0; i < str.length(); i++) {
+                char currentChar = str.charAt(i);
+                if (currentChar != ' ') {
+                    result += currentChar;
+                }
             }
+            
+            System.out.println("Original String: " + str);
+            System.out.println("String without spaces: " + result);
         }
-
-        System.out.println("Original String: " + str);
-        System.out.println("String without spaces: " + result);
     }
 }
