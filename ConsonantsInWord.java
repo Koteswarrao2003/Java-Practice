c0715d295d9ec2388f34d31b7ecb984879547167
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-01-18 21:20:42 +0530

    Update ConsonantsInWord.java

--- ConsonantsInWord.java
+++ ConsonantsInWord.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class ConsonantsInWord{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter a Word: ");
+        System.out.print("Enter any Word: ");
         String str=sc.next();
 
         char ch[]=str.toLowerCase().toCharArray();
