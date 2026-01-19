85cd1e2b4b1c0636d2fec0835144c58d4e31a7e3
Koteswararao8534 <koteswararao8534@gmail.com>
2026-01-19 22:12:32 +0530

    Added creatingList

---
+++ CreatingList.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CreatingList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println("List: " + list);
+    }
+}
