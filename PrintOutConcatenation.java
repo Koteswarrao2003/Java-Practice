5dfa7fb86361c1df374c8cd1c199922da337e702
Koteswararao8534 <koteswararao8534@gmail.com>
2026-02-05 17:03:20 +0530

    Added PrintOutConcatenation

---
+++ PrintOutConcatenation.java
@@ -0,0 +1,10 @@
+public class PrintOutConcatenation{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        System.out.println(a+b+" Result");
+        System.out.println("Result: "+a+b);
+        System.out.println("Result: "+(a+b));
+    }
+}
