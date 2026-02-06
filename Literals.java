ae27f65fe41fcc4686257766837b4a0c66d3bdf3
Koteswararao8534 <koteswararao8534@gmail.com>
2026-02-06 18:44:21 +0530

    Added literals

---
+++ Literals.java
@@ -0,0 +1,32 @@
+public class Literals {
+    public static void main(String[] args) {
+
+        int decimal = 100;
+        int binary = 0b1010;
+        int octal = 012;
+        int hexadecimal = 0xA;
+
+        float floatLiteral = 10.5f;
+        double doubleLiteral = 99.99;
+
+        char charLiteral = 'J';
+        String stringLiteral = "Java";
+
+        boolean trueLiteral = true;
+        boolean falseLiteral = false;
+
+        int underscoreLiteral = 1_00_000;
+
+        System.out.println(decimal);
+        System.out.println(binary);
+        System.out.println(octal);
+        System.out.println(hexadecimal);
+        System.out.println(floatLiteral);
+        System.out.println(doubleLiteral);
+        System.out.println(charLiteral);
+        System.out.println(stringLiteral);
+        System.out.println(trueLiteral);
+        System.out.println(falseLiteral);
+        System.out.println(underscoreLiteral);
+    }
+}
