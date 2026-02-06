aaf40c2f999d819e09fc79fa6b41a8ab700521d6
Koteswararao8534 <koteswararao8534@gmail.com>
2026-02-06 18:42:36 +0530

    Added AllDataTypes

---
+++ AllDataTypes.java
@@ -0,0 +1,29 @@
+public class AllDataTypes {
+    public static void main(String[] args) {
+
+        byte b = 10;
+        short s = 100;
+        int i = 1000;
+        long l = 100000L;
+
+        float f = 10.5f;
+        double d = 99.99;
+
+        char c = 'A';
+        boolean flag = true;
+
+        String str = "Hello Java";
+        int[] arr = {1, 2, 3, 4, 5};
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + flag);
+        System.out.println("String: " + str);
+        System.out.println("Array first element: " + arr[0]);
+    }
+}
