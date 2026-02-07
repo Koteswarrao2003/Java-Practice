64e7837cad72eaa3093e95547199767af7a48cb6
Koteswararao8534 <koteswararao8534@gmail.com>
2026-02-07 19:36:30 +0530

    Added wrapperclasses

---
+++ WrapperClasses.java
@@ -0,0 +1,23 @@
+public class WrapperClasses{
+    public static void main(String[] args) {
+        Byte b=12;
+        Short s=25;
+        Integer i=642;
+        Long l=9983l;
+        Float f=43.2f;
+        Double d=53.56d;
+        Character ch='k';
+        String str="Ram";
+        Boolean bool=false;
+
+        System.out.println("Byte value: "+b);
+        System.out.println("Short value: "+s);
+        System.out.println("Integer value: "+i);
+        System.out.println("Long value: "+l);
+        System.out.println("Float value: "+f);
+        System.out.println("Double value: "+d);
+        System.out.println("Character value: "+ch);
+        System.out.println("String value: "+str);
+        System.out.println("Boolean value: "+bool);
+    }
+}
