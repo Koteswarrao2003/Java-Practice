f58bc5fc99433864cfff2af580977ab77b93b069
Koteswararao8534 <koteswararao8534@gmail.com>
2026-02-07 19:43:08 +0530

    Added datatyperanges

---
+++ DataTypesRanges.java
@@ -0,0 +1,38 @@
+public class DataTypesRanges{
+    public static void main(String[] args) {
+        System.out.println("Byte Range:");
+        System.out.println("Minimum Value:"+Byte.MIN_VALUE);
+        System.out.println("Maximum Value:"+Byte.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("Short Range:");
+        System.out.println("Minimum Value:"+Short.MIN_VALUE);
+        System.out.println("Maximum Value:"+Short.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("Integer Range:");
+        System.out.println("Minimum Value:"+Integer.MIN_VALUE);
+        System.out.println("Maximum Value:"+Integer.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("Float Range:");
+        System.out.println("Minimum Value:"+Float.MIN_VALUE);
+        System.out.println("Maximum Value:"+Float.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("Double Range:");
+        System.out.println("Minimum Value:"+Double.MIN_VALUE);
+        System.out.println("Maximum Value:"+Double.MAX_VALUE);
+
+        System.out.println();
+
+        System.out.println("Character Range:");
+        System.out.println("Minimum Value:"+Character.MIN_VALUE);
+        System.out.println("Maximum Value:"+Character.MAX_VALUE);
+
+    }
+}
