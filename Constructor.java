2f746ba2da1e4ef4cc4fc5b28ff2da0420bd4185
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-02-28 18:21:50 +0530

    Document constructor overloading in Constructor.java
    Added comments explaining constructor overloading in Java.

--- Constructor.java
+++ Constructor.java
@@ -1,3 +1,32 @@
+/*
+CONSTRUCTOR OVERLOADING IN JAVA
+
+This program demonstrates constructors in Java.
+
+Constructor:
+A constructor is a special method 
+that is called when an object is created.
+It is used to initialize variables.
+
+In this program:
+1) Parameterized Constructor:
+   - Takes values for a and b.
+   - Assigns them using 'this' keyword.
+
+2) Default Constructor:
+   - Assigns default values (0 and "default").
+
+Method:
+display() → Prints values of a and b.
+
+In main():
+- obj1 uses parameterized constructor.
+- obj2 uses default constructor.
+
+This is called Constructor Overloading
+(same constructor name, different parameters).
+*/
+
 public class Constructor {
     int a;
     String b;
