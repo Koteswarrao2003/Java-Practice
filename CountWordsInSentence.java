59c4693d8dc1939beffe1c838a78837918b387f1
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-03-01 20:38:24 +0530

    Change input method to accept user sentence
    Modified the program to take user input for the sentence instead of using a hardcoded string.

--- CountWordsInSentence.java
+++ CountWordsInSentence.java
@@ -1,8 +1,29 @@
+/*
+COUNT WORDS IN A SENTENCE
+
+This program counts the number of words
+in a given sentence.
+
+Program Logic:
+1) Take a full sentence as input from user.
+2) Use split(" ") method to separate words
+   based on space.
+3) Store the words in a String array.
+4) Print the length of the array,
+   which represents the number of words.
+
+Note:
+Words are separated based on spaces.
+*/
+
+import java.util.*;
+
 public class CountWordsInSentence {
     public static void main(String[] args) {
-        String sentence = "Hello world this is Java";
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter something:");
+        String sentence = sc.nextLine();
         String []words=sentence.split(" ");
         System.out.println("Number of words in the sentence: " + words.length);
     }
-
 }
