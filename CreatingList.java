70538a38d6152bc44003366a32bf78895bc03f56
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-03-02 21:26:40 +0530

    Update CreatingList.java

--- CreatingList.java
+++ CreatingList.java
@@ -1,3 +1,19 @@
+/*
+CREATING A LIST USING ARRAYLIST
+
+This program demonstrates how to create
+and use a List in Java.
+
+Program Logic:
+1) Create a List using ArrayList.
+2) Add elements using add() method.
+3) Print the list.
+
+List is part of Collection Framework.
+ArrayList is dynamic, meaning its size
+can grow automatically when elements are added.
+*/
+
 import java.util.*;
 public class CreatingList {
     public static void main(String[] args) {
