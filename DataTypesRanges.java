5217edf5ae60b19ba91fa5813a9cd05b4be3f538
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-03-02 21:27:17 +0530

    Update DataTypesRanges.java

--- DataTypesRanges.java
+++ DataTypesRanges.java
@@ -1,3 +1,21 @@
+/*
+DATA TYPES RANGES IN JAVA
+
+This program displays the minimum and maximum
+values of different primitive data types.
+
+Program Logic:
+1) Use wrapper classes like Byte, Short, Integer, 
+   Float, Double, and Character.
+2) Access their predefined constants:
+   - MIN_VALUE → Smallest value
+   - MAX_VALUE → Largest value
+3) Print the range of each data type.
+
+This helps to understand the storage limits
+of different data types in Java.
+*/
+
 public class DataTypesRanges{
     public static void main(String[] args) {
         System.out.println("Byte Range:");
