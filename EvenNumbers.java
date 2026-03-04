668c52e05bb1a58d2ad38b7b99f14e6c342dc22e
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-03-04 22:51:06 +0530

    Update EvenNumbers.java

--- EvenNumbers.java
+++ EvenNumbers.java
@@ -1,3 +1,18 @@
+/*
+PRINT EVEN NUMBERS FROM 1 TO 100
+
+This program prints all even numbers
+between 1 and 100.
+
+Program Logic:
+1) Use a loop from 1 to 100.
+2) Check each number using (number % 2 == 0).
+3) If the condition is true, the number is even.
+4) Print the even number.
+
+Even numbers are divisible by 2.
+*/
+
 public class EvenNumbers{
     public static void main(String[] args) {
         for(int i=1;i<=100;i++){
