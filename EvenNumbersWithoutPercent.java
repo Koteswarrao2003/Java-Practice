2c5d03db62cd7e81e825d5409b0f8dfc6b4a9be8
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-03-05 21:45:13 +0530

    Update EvenNumbersWithoutPercent.java

--- EvenNumbersWithoutPercent.java
+++ EvenNumbersWithoutPercent.java
@@ -1,3 +1,20 @@
+/*
+PRINT EVEN NUMBERS WITHOUT USING % OPERATOR
+
+This program prints even numbers between
+two numbers given by the user.
+
+Program Logic:
+1) Take starting and ending numbers as input.
+2) Use a loop to go from start to end.
+3) Check even number using (i/2)*2 == i.
+4) If the condition is true, the number is even.
+5) Print the even numbers.
+
+This method identifies even numbers
+without using the modulus (%) operator.
+*/
+
 import java.util.Scanner;
 
 public class EvenNumbersWithoutPercent {
