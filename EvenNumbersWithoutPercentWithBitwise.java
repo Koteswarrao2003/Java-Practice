bbe4e22561d87d85799416452b9393d09fc49925
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-03-05 21:46:00 +0530

    Update EvenNumbersWithoutPercentWithBitwise.java

--- EvenNumbersWithoutPercentWithBitwise.java
+++ EvenNumbersWithoutPercentWithBitwise.java
@@ -1,3 +1,22 @@
+/*
+PRINT EVEN NUMBERS USING BITWISE OPERATOR
+
+This program prints even numbers
+between a given range.
+
+Program Logic:
+1) Take starting and ending numbers from user.
+2) Use a loop to iterate from start to end.
+3) Check even number using bitwise operator:
+   (i & 1) == 0
+4) If the condition is true, the number is even.
+5) Print the even numbers.
+
+Note:
+Bitwise AND (&) with 1 checks the last bit.
+If last bit is 0 → Even number.
+*/
+
 import java.util.Scanner;
 
 public class EvenNumbersWithoutPercentWithBitwise {
