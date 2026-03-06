43e3834658b0a20040ff2e4c6dc5853da9260a7e
KOTESWARA RAO KARAMPUDI <koteswararao8534@gmail.com>
2026-03-06 22:02:47 +0530

    Update Factors.java

--- Factors.java
+++ Factors.java
@@ -1,3 +1,19 @@
+/*
+FACTORS OF A NUMBER
+
+Factors are numbers that divide a given
+number completely without leaving a remainder.
+
+Example:
+Factors of 6 → 1, 2, 3, 6
+
+Program Logic:
+1) Take a number as input from the user.
+2) Use a loop from 1 to the given number.
+3) Check if the number is divisible by i (num % i == 0).
+4) If divisible, print that number as a factor.
+*/
+
 import java.util.*;
 public class Factors{
     public static void main(String[] args) {
