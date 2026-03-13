ffc4d6ef864adf24344930034d664017aca4e5d7
KARAMPUDI KOTESWARA RAO <koteswararao8534@gmail.com>
2026-03-13 21:50:00 +0530

    Update MethodOverRiding.java

--- MethodOverRiding.java
+++ MethodOverRiding.java
@@ -1,3 +1,26 @@
+/*
+METHOD OVERRIDING IN JAVA
+
+Method Overriding occurs when a child class
+provides its own implementation of a method
+that is already defined in the parent class.
+
+Program Logic:
+1) Parent class has a method show().
+2) Child class overrides the show() method.
+3) When the method is called, the child class
+   version is executed.
+
+In main():
+- Parent object calls Parent method.
+- Child object calls Child method.
+- Parent reference with Child object demonstrates
+  runtime polymorphism.
+
+The @Override annotation indicates that
+the method is overriding the parent method.
+*/
+
 class Parent {
     void show() {
         System.out.println("Parent class show method");
