158e8e5f105f3e928fe2807f7404618058c92e78
KARAMPUDI KOTESWARA RAO <koteswararao8534@gmail.com>
2026-03-22 20:29:58 +0530

    Update ReversePyramid.java

--- ReversePyramid.java
+++ ReversePyramid.java
@@ -1,3 +1,18 @@
+/*
+REVERSE PYRAMID PATTERN
+
+This program prints an inverted pyramid
+using '*' symbols.
+
+Program Logic:
+1) Loop from n to 1 (top to bottom).
+2) Print spaces (n - i) for alignment.
+3) Print stars (2*i - 1) in each row.
+4) Move to next line after each row.
+
+This forms a reverse pyramid shape.
+*/
+
 public class ReversePyramid{
     public static void main(String[] args) {
         int n=5;
