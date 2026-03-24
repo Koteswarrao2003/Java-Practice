d6dfe5e711a6ac58290effd2e5e779e046d01628
KARAMPUDI KOTESWARA RAO <koteswararao8534@gmail.com>
2026-03-24 19:25:34 +0530

    Update StringLiteralWithIntern.java

--- StringLiteralWithIntern.java
+++ StringLiteralWithIntern.java
@@ -1,3 +1,30 @@
+/*
+STRING INTERN METHOD IN JAVA
+
+This program demonstrates the use of
+intern() method in String.
+
+Program Logic:
+1) String str = "Hello"
+   → Stored in String pool.
+
+2) String s = new String("Hello")
+   → Created in heap memory.
+
+3) s.intern()
+   → Moves reference to String pool.
+
+Comparison:
+- (str == s)
+  → Now both refer to same object in pool → true
+
+- str.equals(s)
+  → Compares content → true
+
+intern() ensures that strings share
+the same memory reference in the pool.
+*/
+
 public class StringLiteralWithIntern{
     public static void main(String[] args) {
         String str="Hello";
