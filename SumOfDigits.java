37ef26cc330bec54046b7db74f1763a03377e641
KARAMPUDI KOTESWARA RAO <koteswararao8534@gmail.com>
2026-03-26 21:30:46 +0530

    Update SumOfDigits.java

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,3 +1,22 @@
+/*
+SUM OF DIGITS PROGRAM
+
+This program calculates the sum
+of digits of a number.
+
+Program Logic:
+1) Take a number from the user.
+2) Extract each digit using % operator.
+3) Add each digit to a sum variable.
+4) Remove the last digit using division (/).
+5) Repeat until the number becomes 0.
+6) Print the final sum.
+
+Example:
+Input: 123
+Output: 1 + 2 + 3 = 6
+*/
+
 import java.util.Scanner;
 public class SumOfDigits{
     public static void main(String[] args) {
