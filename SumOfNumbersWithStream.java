2385db4b01002adbfae078303fa705748ae1b0d7
KARAMPUDI KOTESWARA RAO <koteswararao8534@gmail.com>
2026-03-27 22:15:55 +0530

    Update SumOfNumbersWithStream.java

--- SumOfNumbersWithStream.java
+++ SumOfNumbersWithStream.java
@@ -1,3 +1,20 @@
+/*
+SUM OF NUMBERS USING STREAM API
+
+This program calculates the sum
+of elements in a list using Stream API.
+
+Program Logic:
+1) Create a list of integers.
+2) Convert the list into a stream.
+3) Use mapToInt() to convert Integer to int.
+4) Use sum() method to add all elements.
+5) Print the total sum.
+
+Stream API provides a simple and
+efficient way to perform operations on collections.
+*/
+
 import java.util.*;
 
 public class SumOfNumbersWithStream {
