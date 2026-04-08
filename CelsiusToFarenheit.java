73c9d1568ecb73f41369fe90de7a78a86cbca076
KARAMPUDI KOTESWARA RAO <koteswararao8534@gmail.com>
2026-04-08 21:17:44 +0530

    Update CelsiusToFarenheit.java

--- CelsiusToFarenheit.java
+++ CelsiusToFarenheit.java
@@ -1,4 +1,19 @@
+/*
+CELSIUS TO FAHRENHEIT CONVERSION
 
+This program converts temperature 
+from Celsius to Fahrenheit.
+
+Formula:
+Fahrenheit = (Celsius × 9/5) + 32
+
+Program Steps:
+1) Take Celsius value as input from user.
+2) Apply the conversion formula.
+3) Print the Fahrenheit value.
+
+Scanner is used to read input from user.
+*/
 
 import java.util.Scanner;
 
